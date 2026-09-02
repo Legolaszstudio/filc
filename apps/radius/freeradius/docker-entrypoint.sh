99ef1b79e0aae081762d1d73f9afbe385f9b4123
Legolaszstudio <novotny.levente@gmail.com>
2026-09-02 11:00:38 +0200

    Vibe freeradius

---
+++ apps/radius/freeradius/docker-entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+set -e
+
+PATH=/opt/sbin:/opt/bin:$PATH
+export PATH
+
+# Make configs read only for security
+chmod o-w /opt/etc/raddb/clients.conf
+chmod o-w /opt/etc/raddb/sites-available/default
+chmod o-w /opt/etc/raddb/sites-available/inner-tunnel
+
+# Enable inner-eap module
+chmod o-w /opt/etc/raddb/mods-available/eap
+chmod o-w /opt/etc/raddb/mods-available/inner-eap
+ln -s /opt/etc/raddb/mods-available/inner-eap /opt/etc/raddb/mods-enabled/inner-eap || true
+
+# Enable rest module to connect to Chronos backend
+chmod o-w /opt/etc/raddb/mods-available/rest
+ln -s /opt/etc/raddb/mods-available/rest /opt/etc/raddb/mods-enabled/rest || true
+
+# Secure certs
+chmod o-w /opt/etc/raddb/certs/*.pem
+chmod o-w /opt/etc/raddb/certs/*.key
+chmod o-w /opt/etc/raddb/certs/*.p12
+chmod o-w /opt/etc/raddb/certs/*.crt
+chmod o-w /opt/etc/raddb/certs/*.csr
+chmod o-w /opt/etc/raddb/certs/dh
+
+# Remove unused modules if exist
+rm /opt/etc/raddb/mods-enabled/files || true
+
+# Configure eapol_test template
+if [ -f /eapol_test.conf.template ]; then
+  sed "s/IDHERE/$RADTEST_USER/g" /eapol_test.conf.template | sed -e "s/PASSHERE/$RADTEST_PASS/g" > /eapol_test.conf
+fi
+
+apk update
+apk add wpa_supplicant tzdata
+
+if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
+    set -- radiusd "$@"
+fi
+
+if [ "$1" = 'radiusd' ] || [ "$1" = 'freeradius' ]; then
+    shift
+    exec radiusd -f "$@"
+fi
+
+exec "$@"
